3007219c5dce36af6aa87752d228d5ceccf969eb
Onuralp SEZER <thunderbirdtr@gmail.com>
2023-11-22 03:09:48 +0300

    feat: ✨ slint-ui and basic avd listing added (#1)
    Signed-off-by: Onuralp SEZER <thunderbirdtr@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -16,18 +16,36 @@
  */
 
 use std::collections::LinkedList;
+slint::include_modules!();
+
 
 mod avd_item;
 mod r#const;
 mod avd_funcs;
 mod parser_funcs;
 
-fn main() {
-    let avds: LinkedList<avd_item::AvdItem> = avd_funcs::list_avds();
 
+fn main() -> Result<(), slint::PlatformError> {
+    let avds: LinkedList<avd_item::AvdItem> = avd_funcs::list_avds();
+    let mut avd_names: String = String::new();
     for avd in avds {
-        println!("{}, {}", avd.avd_display_name, avd.userdata_size);
-        let parsed: String = parser_funcs::parse_avd_to_ini(avd);
-        println!("{}", parsed);
+        //println!("{}, {}", avd.avd_display_name, avd.userdata_size);
+        //let parsed: String = parser_funcs::parse_avd_to_ini(avd);
+        avd_names = avd.avd_display_name + "\n" + &avd_names;
+        //println!("{}", parsed);
+        
     }
+
+    let ui = AppWindow::new()?;
+
+    let ui_handle = ui.as_weak();
+    ui.on_request_avd_list(move || {
+        let ui = ui_handle.unwrap();
+        ui.set_avdlist(ui.get_avdlist() + &avd_names);
+    });
+
+    ui.run()
 }
+
+
+
